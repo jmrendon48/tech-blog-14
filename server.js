08b89750638896dda4dcda84f154c61f080cf5a5
AlexDeLarge <jmrendon48@gmail.com>
2021-03-29 03:00:13 -0700

    set time of cookie expiration

--- server.js
+++ server.js
@@ -11,8 +11,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: { expires: 10 * 60 * 1000 },
   resave: false,
+  rolling: true,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize

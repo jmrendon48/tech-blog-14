2567d9116793ff67a32e7f7747ad148055973082
AlexDeLarge <jmrendon48@gmail.com>
2021-03-29 03:04:57 -0700

    revised cookies expires to maxAge

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: { expires: 10 * 60 * 1000 },
+  cookie: { maxAge: 10 * 60 * 1000 },
   resave: false,
   rolling: true,
   saveUninitialized: true,
